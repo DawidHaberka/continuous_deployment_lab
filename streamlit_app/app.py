4bc5b9cc7a842b416b01c3336f2ceb99bf2432e0
DawidHaberka <118928632+DawidHaberka@users.noreply.github.com>
2026-04-29 23:32:07 +0200

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Hello, Streamlit!")
-    st.write("O kurwa ten tytuł jest po polsku!")
+    st.write("O kurwa ten tytuł jest po polsku! LOL")
 
 if __name__ == "__main__":
     main()
