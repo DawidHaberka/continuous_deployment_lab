7d43b046f3c5cb64e39e154410a95c970ce03c01
DawidHaberka <118928632+DawidHaberka@users.noreply.github.com>
2026-04-30 16:58:42 +0200

    Fix formatting of Polish text in app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Hello, Streamlit!")
-    st.write("O kurwa ten tytuł jest po polsku! LOL I co teraz jak se zmienie to to zadziała i huj?" xd)
+    st.write("O kurwa ten tytuł jest po polsku! LOL I co teraz jak se zmienie to to zadziała i huj? xd")
 
 if __name__ == "__main__":
     main()
